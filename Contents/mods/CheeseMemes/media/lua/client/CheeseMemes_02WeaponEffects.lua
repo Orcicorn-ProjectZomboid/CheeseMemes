5981d8048ea0e85833ad7e356d9a29cdbc07616e
Orcicorn <orcicorn@gmail.com>
2023-02-08 21:18:55 -0500

    Randomize PvE Axl Sound

--- Contents/mods/CheeseMemes/media/lua/client/CheeseMemes_02WeaponEffects.lua
+++ Contents/mods/CheeseMemes/media/lua/client/CheeseMemes_02WeaponEffects.lua
@@ -69,7 +69,7 @@ CheeseMemes.Weapons.MeltSomeFaces = function(target, player)
         if ZombRand(200) == 69 then
             target:setStaggerBack(true);
             target:SetOnFire();
-            CheeseMemes.Functions.EmitSound(player, "AxelRoseProc1");
+            CheeseMemes.Functions.EmitSound(player, "AxelRoseProc" .. ZombRand(1,2));
         end
     elseif instanceof(target, "IsoPlayer") then 
         if ZombRand(100) <= 25 then
