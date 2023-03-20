28fe65a757196e62acfb4c96dade3a96dceb56e0
Orcicorn <orcicorn@gmail.com>
2023-03-20 12:26:19 -0400

    Couple more cheese quotes

--- Contents/mods/CheeseMemes/media/lua/client/CheeseMemes_03ItemEffects.lua
+++ Contents/mods/CheeseMemes/media/lua/client/CheeseMemes_03ItemEffects.lua
@@ -24,7 +24,7 @@ local lastGnomeSniff = 0;               -- Last time you sniffed David
     @trigger    CheeseMemes.Events.OnHitEvents
 ]]
 CheeseMemes.Items.GamerGlasses = function(player)
-    local maxQuotes = 59;                -- IGUI_CheeseQuotesXXX
+    local maxQuotes = 61;                -- IGUI_CheeseQuotesXXX
     if ZombRand(1, 100) <= 1 then 
         local quote = ZombRand(0, (maxQuotes + 1))
         if quote < 1 then quote = 1; end
